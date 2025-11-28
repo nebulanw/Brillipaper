74011f9388fcd8b6f84639d78facac4b8d9faa2e
Harry Liu <nebulanw@nebulanw.dev>
2025-11-27 16:08:31 -0800

    feat: remove SDK import, Countly, Sentry (submodule bump)

--- Program.cs
+++ Program.cs
@@ -13,11 +13,7 @@ namespace Gh.Walliant
     [STAThread]
     private static void Main()
     {
-      new Runner("walliant", "Local\\{E8C20855-465C-411B-A0F9-6897240D9EB3}", "563f6dee0d52d0dd7c1a7cc4b9df5626364d2d5b", "https://25e5cccd5e35435e9399507e39a52b6d@o357035.ingest.sentry.io/5209089", (ConfigBase) Config.Instance).Run(new Runner.FormCreator(Program.CreateForm), new Warning()
-      {
-        Title = "Do you want to disable web indexing?",
-        Text = "Without web indexing, the best wallpaper views won’t be available.\r\nKeep it enabled to enjoy the app FOR FREE!"
-      });
+      new Runner("walliant", "Local\\{E8C20855-465C-411B-A0F9-6897240D9EB3}", (ConfigBase) Config.Instance).Run(new Runner.FormCreator(Program.CreateForm));
     }
 
     private static Form CreateForm()
